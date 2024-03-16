1e0b7d5c6d45dcd780d8f85dbfaba5fb4a8001ba
Matheus2237 <mathpaulinor@gmail.com>
2024-03-15 21:41:09 -0300

    fix: Adequação dos includes nos arquivos

--- include/List.h
+++ include/List.h
@@ -1,28 +1,29 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <type_traits>
+
 template <typename T, unsigned int capacity>
 class List {
 
-static_assert(std::is_arithmetic<T>::value && !std::is_same<T, bool>::value && !std::is_same<T, char>::value, "Classe aceita apenas tipos numericos");
+    static_assert(std::is_arithmetic<T>::value && !std::is_same<T, bool>::value && !std::is_same<T, char>::value, "Classe aceita apenas tipos numericos");
 
-private:
-    const unsigned int MAX_LIST;
-    T Entry[capacity+1];
-    unsigned int count;
+    private:
+        const unsigned int MAX_LIST;
+        T Entry[capacity+1];
+        unsigned int count;
 
-public:
-    List();
-    ~List();
-    bool empty();
-    bool full();
-    void insert(int pos, T entry);
-    void delete_value(int pos, T& entry);
-    void retrieve(int pos, T& entry);
-    void replace(int pos, T entry);
-    void clear();
-    unsigned int size();
-    virtual void sort() = 0;
+    public:
+        List();
+        ~List();
+        bool empty();
+        bool full();
+        void insert(int pos, T entry);
+        void delete_value(int pos, T& entry);
+        void retrieve(int pos, T& entry);
+        void replace(int pos, T entry);
+        void clear();
+        unsigned int size();
 };
 
 #include "../src/List.cpp"
